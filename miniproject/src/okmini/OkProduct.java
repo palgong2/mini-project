63dcbd0778e724c79f08195d3af58acc9ae9714c
DESKTOP-DAK4QA3\okjunseo <dhrwnstj@naver.com>
2024-11-29 01:21:16 +0900

    각자가작업할패키지커밋

---
+++ miniproject/src/okmini/OkProduct.java
@@ -0,0 +1,29 @@
+package okmini;
+
+public class OkProduct {
+    private String name;
+    private String brand;
+    private int volume; // 용량 (ml)
+    private int quantity; // 재고 개수
+    private String description;
+
+    public OkProduct(String name, String brand, int volume, int quantity, String description) {
+        this.name = name;
+        this.brand = brand;
+        this.volume = volume;
+        this.quantity = quantity;
+        this.description = description;
+    }
+
+    public void printDetails() {
+        System.out.println("제품명: " + name);
+        System.out.println("브랜드: " + brand);
+        System.out.println("용량: " + volume + "ml");
+        System.out.println("재고 개수: " + quantity);
+        System.out.println("설명: " + description);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
