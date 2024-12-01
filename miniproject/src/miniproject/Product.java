10b0095e6a3fd103ea3b3f0c00d54722060272a9
JUNSEO <dhrwnstj@naver.com>
2024-12-01 16:25:56 +0900

    miniproject 변경

--- miniproject/src/miniproject/Product.java
+++ miniproject/src/miniproject/Product.java
@@ -23,56 +23,18 @@ public class Product {
         System.out.println("용량: " + volume + "ml");
         System.out.println("재고 개수: " + quantity);
         System.out.println("설명: " + description);
-        System.out.println("피부Type: " + skinType);
+        System.out.println("피부타입: " + skinType);
     }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public int getQuantity() {
+        return quantity;
+    }
 
-	public String getBrand() {
-		return brand;
-	}
-
-	public void setBrand(String brand) {
-		this.brand = brand;
-	}
-
-	public int getVolume() {
-		return volume;
-	}
-
-	public void setVolume(int volume) {
-		this.volume = volume;
-	}
-
-	public int getQuantity() {
-		return quantity;
-	}
-
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public String getSkinType() {
-		return skinType;
-	}
-
-	public void setSkinType(String skinType) {
-		this.skinType = skinType;
-	}
-
-
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
